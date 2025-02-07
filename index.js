b1954cadde630be8783ac484d1f3c6c0986a0f4a
hooved <172129504+hooved@users.noreply.github.com>
2025-02-07 09:12:35 -0500

    try preventing autozoom on prompt

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ window.BACKEND = (normalizedParams["BACKEND"] === "WASM") ? "WASM" : "WebGPU";
 const isMobileAgent = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
 const hasTouchScreen = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
 window.isMobile = isMobileAgent || hasTouchScreen;
+if (window.isMobile) document.documentElement.classList.add('mobile'); // prevent annoying auto-zoom when entering prompt on mobile
 window.TEST = normalizedParams["TEST"];
 
 const tiktokenReady = (async () => {
